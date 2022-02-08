8d3ff2df24a5a4ed0f24b9c217fb8c5577281887
Nicolaspwilde <69107250+Nicolaspwilde@users.noreply.github.com>
2022-02-08 22:56:18 +0530

    Update pyramid after 180 rotation.cpp
    Updates

--- pyramid after 180 rotation.cpp
+++ pyramid after 180 rotation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the number of pyramid ";
+    cout<<"Enter the number of Rows ";
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
