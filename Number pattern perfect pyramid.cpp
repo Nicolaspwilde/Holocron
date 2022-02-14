8de56876ae535b4c6e534e1f9e520345b0c580e6
Nicolaspwilde <69107250+Nicolaspwilde@users.noreply.github.com>
2022-02-14 23:19:07 +0530

    New add on

--- Number pattern perfect pyramid.cpp
+++ Number pattern perfect pyramid.cpp
@@ -2,15 +2,12 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"Enter the number";
-    cin>>n;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=1;i++){
         for(int j=1;j<=n-i;j++){
             cout<<" ";
         }
         for(int j=1;j<=i;j++){
-            cout<<j<<" ";
+            cout<<"*";
         }
     cout<<endl;
     }
--- Rhombus Varient.cpp
+++ Rhombus Varient.cpp
@@ -3,16 +3,17 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the number";
+    cout<<"enter the numb";
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
+        for(int j=1;j>=n;j--){
             cout<<" ";
         }
-        for(int j=1;j<=i;j++){
-            cout<<j<<" ";
+        for(int j=1;j<=n;j++){
+            cout<<"*";
         }
     cout<<endl;
     }
 return 0;
+
 }
