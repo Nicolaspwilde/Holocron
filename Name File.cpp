f2dd9587cd0a7c5a3d9f1b5c925692f30d0c3671
Nicolaspwilde <69107250+Nicolaspwilde@users.noreply.github.com>
2022-06-30 00:00:03 +0530

    New update 29-06-2022
    New update 29-06-2022

--- Name File.cpp
+++ Name File.cpp
@@ -5,11 +5,11 @@ int main()
     int A1,B1,Sum;
     cout<<"Hello World"<<endl;
     cout<<"Abhimanyu"<<endl;
+
     cout<<"Enter the number A1" <<endl;
     cin>>A1;
     cout<<"Enter the number B1" <<endl;
     cin>>B1;
     Sum = A1 + B1;
-    cout<<"Sum of"<<A1<<"+"<<B1<<"="<<Sum ;
     return 0;
 }
