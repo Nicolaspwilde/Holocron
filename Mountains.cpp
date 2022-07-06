65b5891ce5fe3efef4a50e20d8d13d043892641e
Nicolaspwilde <69107250+Nicolaspwilde@users.noreply.github.com>
2022-07-07 00:11:41 +0530

    n

--- Mountains.cpp
+++ Mountains.cpp
@@ -4,13 +4,10 @@ int main ()
 {
 int n;
 cin>>n;
-for(int i=1;i<=3;i++){
-        for(int j=1;j<=n;j++){
-                if(((i+j)%4==0)||(i==2 && j%4==0)){
+for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+                if((i+j)%4==0||(i==2 && j%4==0)){
                         cout<<" * ";
-                }
-                else{
-                        cout<<"   ";
                 }
                 
         }
