61bb00125bba25217329a51e181fe0142c956f47
Nicolaspwilde <69107250+Nicolaspwilde@users.noreply.github.com>
2022-07-16 09:39:16 +0530

    start

---
+++ decimal to octal (Func).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int bin(int n){
+  
+  int i=1;
+  int rem=0;
+  int ans=0;
+  while(n>0){
+    rem=n%8;
+    n=n/8;
+    ans= ans+rem*i;
+    i=i*10;
+  }
+  return ans;
+}
+int main() {
+  int n;
+  cin>>n;
+  cout<<bin(n);
+  return 0;
+}
