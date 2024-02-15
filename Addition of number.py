6a5e6016d9bfd6e126a998246c79b06476f9d2ae
Abhi <asinghnegi55@gmail.com>
2024-02-15 12:25:07 +0530

    Start Again

---
+++ Addition of number.py
@@ -0,0 +1,10 @@
+def add_numbers(n):
+    total = 0
+    for i in range(n):
+        num = float(input("Enter number {}: ".format(i+1)))
+        total += num
+    return total
+
+n = int(input("How many numbers do you want to add? "))
+result = add_numbers(n)
+print("The sum of the {} numbers is: {}".format(n, result))
