d691ccbf53c00f6b97e170abd3fed0beddcbb6ad
Abhimanyu <asinghnegi55@gmail.com>
2025-07-24 11:23:30 +0530

    Commit via vs code

---
+++ Excercise-6.py
@@ -0,0 +1,3 @@
+My_information = {'First_name' : 'Abhimanyu','Middle_name': 'Singh','Last_name':'Negi','Age':25,'city':'Delhi'}
+print(f"My name is {My_information['First_name']} {My_information['Middle_name']} {My_information['Last_name']}")
+
